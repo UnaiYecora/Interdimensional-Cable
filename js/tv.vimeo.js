5112b252068369099f263f96180d6952fef6e874
UnaiYecora <unaiyecora@gmail.com>
2017-10-27 05:13:14 +0100

    Cleaning code 3

--- js/tv.vimeo.js
+++ js/tv.vimeo.js
@@ -6,41 +6,41 @@ var vimeo = {
     obj: null, //will reference the current vimeo embed
 
     //sends request for paused state or reacts to paused state
-    togglePlay: function(paused){
-        if(paused === undefined){
+    togglePlay: function(paused) {
+        if (paused === undefined) {
             var msg = '{"method": "paused"}';
             vimeo.sendMsg(msg);
-        }else{
-            if(paused){
+        } else {
+            if (paused) {
                 vimeo.play();
-            }else{
+            } else {
                 vimeo.pause();
             }
         }
     },
 
-    play: function(){
+    play: function() {
         var msg = '{"method": "play"}';
         vimeo.sendMsg(msg);
     },
 
-    pause: function(){
+    pause: function() {
         var msg = '{"method": "pause"}';
         vimeo.sendMsg(msg);
     },
 
-    createEmbed: function(url){
+    createEmbed: function(url) {
         var ID, created = {};
         ID = url.match(/\/(\d+)/);
 
-        if(ID){
+        if (ID) {
             ID = ID[1];
         }
-        if(ID){
-            created.embed = "&lt;iframe src=\"http://player.vimeo.com/video/"+ID+"\" width=\"600\" height=\"338\" frameborder=\"0\" webkitallowfullscreen allowfullscreen&gt;&lt;/iframe&gt;";
+        if (ID) {
+            created.embed = "&lt;iframe src=\"http://player.vimeo.com/video/" + ID + "\" width=\"600\" height=\"338\" frameborder=\"0\" webkitallowfullscreen allowfullscreen&gt;&lt;/iframe&gt;";
             created.thumbnail = null;
             return created;
-        }else{
+        } else {
             return false;
         }
     },
@@ -49,61 +49,61 @@ var vimeo = {
     prepEmbed: function(embed) {
         var split, js_str = '?api=1&player_id=vimeoplayer';
 
-        if(embed.indexOf('" width="') !== -1){
+        if (embed.indexOf('" width="') !== -1) {
             split = embed.indexOf('" width="');
-            embed = embed.substr(0,split)+js_str+embed.substr(split);
+            embed = embed.substr(0, split) + js_str + embed.substr(split);
 
             split = embed.indexOf('src="');
-            embed = embed.substr(0,split)+'id="vimeoplayer" '+embed.substr(split);
+            embed = embed.substr(0, split) + 'id="vimeoplayer" ' + embed.substr(split);
         }
 
         return embed;
     },
 
-    readyListener: function(){
+    readyListener: function() {
         //register finish listener
         var msg = '{"method": "addEventListener", "value": "finish"}';
         vimeo.sendMsg(msg);
 
         //auto play if enabled
-        if(Globals.auto){
+        if (Globals.auto) {
             vimeo.play();
         }
     },
 
-    finishListener: function(){
-        if(Globals.auto){
+    finishListener: function() {
+        if (Globals.auto) {
             loadVideo('next');
         }
     },
 
-    addListeners: function(){
+    addListeners: function() {
         vimeo.obj = document.getElementById("vimeoplayer");
         window.addEventListener("message", vimeo.receiveMessage, false);
     },
 
-    sendMsg: function(msg){
+    sendMsg: function(msg) {
         var target = vimeo.obj.contentWindow;
         target.postMessage(msg, 'http://player.vimeo.com/');
     },
 
-    receiveMessage: function(msg){
+    receiveMessage: function(msg) {
         var msgData = eval('(' + msg.data + ')');
         var the_switch = (msgData.event === undefined ? msgData.method : msgData.event);
 
-        switch(the_switch){
-        case "ready":
-            vimeo.readyListener();
-            break;
-        case "finish":
-            vimeo.finishListener();
-            break;
-        case "paused":
-            vimeo.togglePlay(msgData.value);
-            break;
-        default:
-            consoleLog(msgData);
-            break;
+        switch (the_switch) {
+            case "ready":
+                vimeo.readyListener();
+                break;
+            case "finish":
+                vimeo.finishListener();
+                break;
+            case "paused":
+                vimeo.togglePlay(msgData.value);
+                break;
+            default:
+                consoleLog(msgData);
+                break;
         }
     }
 };
